7b1f74d2aefba09ddae4e46e9ea61820a29538f4
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-08-22 19:56:46 +0530

    v2 - input validation

--- src/java/com/kelaniya/uni/v2/Main.java
+++ src/java/com/kelaniya/uni/v2/Main.java
@@ -2,6 +2,15 @@ package com.kelaniya.uni.v2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world1");
+        if(args.length == 0){
+            System.out.println("Please provide the operation as an argument");
+            return;
+        }
+        String operation = args[0];
+        if(!( operation.equals("add") || operation.equals("sub") || operation.equals("mul") || operation.equals("div"))){
+            System.out.println("Please proivde add, sub , mul , div as operator argument");
+            return;
+        }
+        System.out.println(args[0]);
     }
 }
